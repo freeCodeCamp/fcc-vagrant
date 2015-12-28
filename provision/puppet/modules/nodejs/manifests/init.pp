35a3977ad4fd9250f096be28e4465544bfd23c04
afrolionx <mike@mikecorp.ca>
2015-12-28 05:06:41 -0500

    made use of $home variable

--- provision/puppet/modules/nodejs/manifests/init.pp
+++ provision/puppet/modules/nodejs/manifests/init.pp
@@ -2,6 +2,7 @@ class nodejs(
   $version  = $nodejs::globals::node_version,
   $bin      = $nodejs::globals::default_bin,
   $user     = $nodejs::globals::default_user
+  $home     = $nodejs::globals::$home_dir
 ) inherits nodejs::globals {
   include stdlib
 
@@ -10,7 +11,7 @@ class nodejs(
   # INFO: install dir is /.nvm
   exec { 'nodejs::install_nvm':
     user    => $user,
-    command => 'curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | NVM_DIR=/home/vagrant/.nvm bash',
+    command => "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | NVM_DIR=${home}/.nvm bash",
     path    => $bin,
     require => [Package['git'], Package['curl']]
   }
@@ -18,7 +19,7 @@ class nodejs(
   # Use nvm to install node
   exec { 'nodejs::nvm_install_node':
     user      => $user,
-    command   => "bash -c 'source /home/vagrant/.nvm/nvm.sh ; nvm install ${version}'",
+    command   => "bash -c 'source ${home}/.nvm/nvm.sh ; nvm install ${version}'",
     path      => $bin,
     # unless    => "bash -c 'source /home/vagrant/.nvm/nvm.sh ; nvm list | grep node | grep v4.2.2'",
     logoutput => on_failure,
@@ -28,7 +29,7 @@ class nodejs(
   # use nvm node version
   exec { 'nodejs::nvm_use_node':
     user      => $user,
-    command   => "bash -c 'source /home/vagrant/.nvm/nvm.sh ; nvm use v${version}'",
+    command   => "bash -c 'source ${home}/.nvm/nvm.sh ; nvm use v${version}'",
     path      => $bin,
     # unless    => "bash -c 'source /home/vagrant/.nvm/nvm.sh ; nvm list | grep node | grep v4.2.2'",
     logoutput => on_failure,
@@ -37,17 +38,17 @@ class nodejs(
 
   # Add nvm to .bashrc
   file_line { 'nodejs::nvm_profile_config_env':
-    path    => '/home/vagrant/.bashrc',
+    path    => "${home}/.bashrc",
     # line    => 'export NVM_DIR="$NVM_DIR"',
-    line    => 'export NVM_DIR="/home/vagrant/.nvm"',
+    line    => "export NVM_DIR=\"${home}/.nvm\"",
     require => [Exec['nodejs::nvm_install_node']]
   }->
   file_line { 'nodejs::nvm_profile_config_path':
-    path    => '/home/vagrant/.bashrc',
+    path    => "${home}/.bashrc",
     line    => '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm'
   }->
   file_line { 'nodejs::nvm_profile_config_path_node':
-    path    => '/home/vagrant/.bashrc',
-    line    => "export PATH=\$PATH:/home/vagrant/.nvm/versions/node/v${version}/bin"
+    path    => "${home}/.bashrc",
+    line    => "export PATH=\$PATH:${home}/.nvm/versions/node/v${version}/bin"
   }
 }
