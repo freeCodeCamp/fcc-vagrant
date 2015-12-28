530b697120d22b91388b2984fd6fc36e19f9a3de
afrolionx <mike@mikecorp.ca>
2015-12-28 04:58:00 -0500

    created global variables for easier editing

--- provision/puppet/manifests/main.pp
+++ provision/puppet/manifests/main.pp
@@ -1,65 +1,61 @@
-# class user_creation {
-#   $pass = "password"
-#   $salt = "somesalt"
+# INFO: included modules
+include othertools
 
-#   user { "fccuser":
-#     name => 'fccuser', # redundant
-#     ensure => 'present',
-#     # uid => '', # auto generated
-#     shell => '/bin/bash',
-#     home => '/home/fccuser',
-#     comment => 'safe user',
-#     password => inline_template("<%= '$pass'.crypt('\$6$$salt') %>"),
-#     managehome => true,
-#     gid => 'vagrant',
-#     groups => [],
-#   }
-# }
+# INFO: Puppet Config
+# INFO: List out provisioning stages
+stage { 'pre': before => Stage['main'] }
+stage { 'post': require => Stage['main'] }
 
-class apt_update {
-  exec { "aptGetUpdate":
-    command => "sudo apt-get update",
-    path => ["/bin", "/usr/bin"]
+# INFO: Global variables
+$node_version   = '4.2.2'
+$home_dir       = '/home/vagrant'
+$bin            = '/usr/local/bin:/usr/bin:/bin'
+$nvm_path       = [
+  "/usr/local/bin",
+  "/usr/bin",
+  "/bin",
+  "${home_dir}/.nvm/versions/node/v${node_version}/bin"
+]
+$default_user   = 'vagrant'
+
+# INFO: List classes in stages
+class { 'aptGetUpdate': stage => pre }
+# class { 'othertools': stage => pre }
+# class { 'nodejs': stage => main }
+class { 'mongodb': stage => main }
+class { 'fccmodule':
+  node_version  => $node_version,
+  home_dir      => $home_dir,
+  default_bin   => $bin,
+  nvm_path      => $nvm_path,
+  default_user  => $default_user,
+  stage => post
+}
+
+# INFO: add aptGetUpdate to catalog
+class aptGetUpdate {
+  exec { 'aptUpdate':
+    command => 'sudo apt-get update',
+    path => '/bin:/usr/bin'
   }
 }
 
+# INFO: add mongodb with params to catalog
 class mongodb {
-  class {'::mongodb::globals':
+  class { '::mongodb::globals':
     manage_package_repo => true,
     bind_ip             => ["127.0.0.1"],
   }->
-  class {'::mongodb::server':
+  class { '::mongodb::server':
     port    => 27017,
     verbose => true,
     ensure  => "present"
   }->
-  class {'::mongodb::client': }
+  class { '::mongodb::client': }
 }
 
-# List out provisioning stages
-stage { 'pre': before => Stage['main'] }
-stage { 'post': require => Stage['main'] }
-
-# List classes in stages
-class { 'apt_update':
-  stage => pre
-}
-class { 'othertools':
-  stage => pre
-}
-class { 'nodejs':
-  stage => main
-}
-class { 'mongodb':
-  stage => main
-}
-class { 'freecodecamp':
-  stage => post
-}
-
-include apt_update
-include othertools
-include nodejs
-include mongodb
-include fccmodule
-#include user_creation
+# INFO: add nodejs with set globals to catalog
+class { 'nodejs::globals':
+  version => $node_version
+}->
+class { 'nodejs': }
