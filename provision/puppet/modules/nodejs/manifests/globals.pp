dc4a99b656c49a987e54a39bfb153936d6111c9e
afrolionx <mike@mikecorp.ca>
2015-12-28 05:06:25 -0500

    added a $home variable

--- provision/puppet/modules/nodejs/manifests/globals.pp
+++ provision/puppet/modules/nodejs/manifests/globals.pp
@@ -1,9 +1,11 @@
 class nodejs::globals (
   $bin      = undef,
   $user     = undef,
-  $version  = undef
+  $version  = undef,
+  $home     = undef
 ) {
-  $default_bin = pick($bin, '/usr/local/bin:/usr/bin:/bin')
+  $default_bin  = pick($bin, '/usr/local/bin:/usr/bin:/bin')
   $default_user = pick($user, 'vagrant')
   $node_version = pick($version, '4.2.2')
+  $home_dir     = pick($home, "/home/${default_user}")
 }
